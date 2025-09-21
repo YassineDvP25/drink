59f710d272949c5c7aa4639aa347ae66af2d5497
Yassine El Anssari <yassinedvp25@gmail.com>
2025-09-21 04:28:57 +0100

    Add drink detail screen and update drink model/components

---
+++ lib/screens/drink_detail_screen.dart
@@ -0,0 +1,86 @@
+import 'package:drinks/models/drink_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DrinkDetailScreen extends StatefulWidget {
+  final int index;
+  const DrinkDetailScreen({super.key, required this.index});
+
+  @override
+  State<DrinkDetailScreen> createState() => _DrinkDetailScreenState();
+}
+
+class _DrinkDetailScreenState extends State<DrinkDetailScreen> {
+  PageController controller = PageController(viewportFraction: 0.50);
+  double currentPage = 0;
+
+  @override
+  void initState() {
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page ?? 1;
+      });
+    });
+    super.initState();
+  }
+
+  final drinks = DrinkModel.drinksList;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            scrollDirection: Axis.horizontal,
+            controller: controller,
+            itemCount: drinks.length,
+            itemBuilder: (context, index) {
+              final scale = 1 - (currentPage - index).abs() * 1;
+              final transtaleY = 1 - (currentPage - index).abs() * 70;
+              final transtaleX = 1 - (currentPage - index).abs() * 70;
+              return Transform.translate(
+                offset: Offset(transtaleY, transtaleX),
+
+                child: Transform.scale(
+                  scale: scale.clamp(0.5, 1),
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Image.asset(
+                            drinks[index].image,
+                            fit: BoxFit.contain,
+                            height: 800,
+                          ),
+                          Positioned(
+                            left: 60,
+                            bottom: index == 0 ? 160 : 180,
+                            child: Container(
+                              height: 8.h,
+                              width: 100.w,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                boxShadow: [
+                                  BoxShadow(
+                                    spreadRadius: 1,
+                                    offset: Offset.zero,
+                                    blurRadius: 36.r,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,31 +1,65 @@
 import 'package:drinks/components/drink_item.dart';
 import 'package:drinks/helpers/spacing.dart';
 import 'package:drinks/models/drink_model.dart';
+import 'package:drinks/screens/drink_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ScrollController controller = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 10).r,
+        padding: EdgeInsets.only(left: 10, right: 10, top: 20).r,
         child: Column(
           children: [
-            verticalSpace(50),
             Expanded(
               child: ListView.builder(
+                controller: controller,
                 itemCount: DrinkModel.drinksList.length,
 
                 itemBuilder: (context, index) {
                   var drink = DrinkModel.drinksList[index];
-                  return DrinkItem(
-                    itemImage: drink.image,
-                    itemName: drink.name,
-                    itemPrice: drink.prix,
+                  return AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, child) {
+                      double offset = 0.0;
+                      if (controller.hasClients) {
+                        offset = controller.offset / 200 - index;
+                      }
+                      offset = offset.clamp(0, 1);
+                      return Transform.scale(
+                        scale: 1 - (offset * 0.2),
+
+                        child: DrinkItem(
+                          itemImage: drink.image,
+                          itemName: drink.name,
+                          itemPrice: drink.prix,
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                
+                                builder: (context) {
+                                  return DrinkDetailScreen(
+                                    index: index,
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    },
                   );
                 },
               ),
